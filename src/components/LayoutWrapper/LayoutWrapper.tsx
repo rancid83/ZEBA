eb1cc03eb2dd46f97671e0a5baa8df64eb0be49f
taylor <taylor@snaps.com>
2025-11-10 01:18:00 +0900

    feat: 색상변경

--- src/components/LayoutWrapper/LayoutWrapper.tsx
+++ src/components/LayoutWrapper/LayoutWrapper.tsx
@@ -3,14 +3,28 @@
 import styles from './LayoutWrapper.module.scss';
 import { useStore } from '@/store';
 import LoadingAnalyzing from '@/components/LoadingAnalyzing/LoadingAnalyzing';
+import { ConfigProvider } from 'antd';
 
 const LayoutWrapper = (props: any) => {
   const { isLoading } = useStore();
 
   return (
-    <>
+    <ConfigProvider
+      theme={{
+        components: {
+          Tabs: {
+            horizontalMargin: '0',
+          },
+        },
+        token: {
+          colorPrimary: '#007676',
+          colorInfo: '#007676',
+          fontSize: 12,
+        },
+      }}
+    >
       {props.children} {isLoading && <LoadingAnalyzing />}
-    </>
+    </ConfigProvider>
   );
 };
 
