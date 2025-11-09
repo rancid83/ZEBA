f031118b0555553679c3c075afbf3c462e816e50
taylor <taylor@snaps.com>
2025-11-10 02:21:57 +0900

    feat: 제거.

--- src/app/[lang]/main/page.tsx
+++ src/app/[lang]/main/page.tsx
@@ -1,7 +1,6 @@
 import MainPage from '@/components/main/Main';
 
 const Page = async (props: any) => {
-  await new Promise((resolve) => setTimeout(resolve, 1000 * 5));
   return <MainPage />;
 };
 
