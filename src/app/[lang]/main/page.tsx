d17ca9bd4479b1c6311f26e54720df523fa36f70
taylor <taylor@snaps.com>
2025-11-10 21:37:41 +0900

    feat: 파비콘 적용

--- src/app/[lang]/main/page.tsx
+++ src/app/[lang]/main/page.tsx
@@ -1,7 +1,7 @@
 import MainPage from '@/components/main/Main';
 
 const Page = async (props: any) => {
-    await new Promise((resolve) => setTimeout(resolve, 1000 * 5));
+  await new Promise((resolve) => setTimeout(resolve, 1000 * 1.5));
   return <MainPage />;
 };
 
